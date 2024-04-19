194287051d7dc0988985f89770d5b1914bd8fbe3
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-04-19 15:20:13 -0400

    refactor: change addUser, getUserWithID, getUserWithEmail functions to retrieve info from database

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -8,7 +8,6 @@ const pool = new Pool({
   database: 'lightbnb',
 });
 
-pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
 /// Users
 
 /**
@@ -17,14 +16,17 @@ pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.l
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
+  return pool
+  .query('SELECT * FROM users WHERE email = $1', [email])
+  .then((res) => {
+    if(!res.rows.length){
+      return(null)
     }
-  }
-  return Promise.resolve(resolvedUser);
+    return res.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  })
 };
 
 /**
@@ -33,7 +35,17 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+  .query('SELECT * FROM users WHERE id = $1', [id])
+  .then((res) => {
+    if(!res.rows.length){
+      return(null)
+    }
+    return res.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  })
 };
 
 /**
@@ -42,10 +54,21 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const {name, email, password } = user;
+  return pool
+  .query(`INSERT INTO users(name, email, password)
+          VALUES ($1, $2, $3) 
+          RETURNING *`, [name, email, password])
+  .then((res) => {
+    if(!res.rows.length){
+      return(null)
+    }
+    return res.rows[0];
+    })
+  .catch((err) => {
+    console.log(err.message);
+    });
+  
 };
 
 /// Reservations
