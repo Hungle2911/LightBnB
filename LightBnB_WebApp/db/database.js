4dc0c22cc00e6ab2c574c6decbb372948bf941ff
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-04-20 16:39:25 -0400

    refactor: getAllReservations function to query lightbnb database for real reservations associated with a specific user.

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -2,10 +2,10 @@ const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 const {Pool} = require('pg');
 const pool = new Pool({
-  user: 'thuvo',
-  password: '123456development',
-  host: 'localhost',
-  database: 'lightbnb',
+  user: "williamlee",
+  password: "development",
+  host: "localhost",
+  database: "lightbnb",
 });
 
 /// Users
@@ -79,7 +79,19 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT properties.* FROM properties 
+            JOIN reservations ON properties.id = property_id
+            WHERE guest_id = $1
+            LIMIT $2 
+            `, [guest_id, limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /// Properties
